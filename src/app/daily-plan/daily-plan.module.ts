c401a3e9c8d7cca5904b2ff1ac5066226a769218
Stefano Elia <elia.ste92@gmail.com>
2019-11-09 16:45:00 +0100

    scss revisitedpart2

--- src/app/daily-plan/daily-plan.module.ts
+++ src/app/daily-plan/daily-plan.module.ts
@@ -1,40 +1,58 @@
-import { AppRoutingModule } from './../app-routing.module';
-import { DailyPlanComponent } from './daily-plan.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { PlanEditComponent } from './plan-edit/plan-edit.component';
-import { PlanRunnerComponent } from './plan-runner/plan-runner.component';
-import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { AppRoutingModule } from "./../app-routing.module";
+import { DailyPlanComponent } from "./daily-plan.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { PlanEditComponent } from "./plan-edit/plan-edit.component";
+import { PlanRunnerComponent } from "./plan-runner/plan-runner.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatFormFieldModule } from "@angular/material/form-field";
 import {
-  MatToolbarModule, MatStepperModule, MatInputModule,
-  MatButtonModule, MatIconModule, MatListModule,
-  MatSnackBarModule, MatExpansionModule, MatTooltipModule, MatNativeDateModule, MatMenuModule
-} from '@angular/material';
-import { PlanListComponent } from './plan-list/plan-list.component';
-
+    MatToolbarModule,
+    MatStepperModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatSnackBarModule,
+    MatExpansionModule,
+    MatTooltipModule,
+    MatNativeDateModule,
+    MatMenuModule,
+    MatCheckboxModule
+} from "@angular/material";
+import { PlanListComponent } from "./plan-list/plan-list.component";
 
 @NgModule({
-  declarations: [PlanEditComponent, PlanRunnerComponent, FooterComponent, NavbarComponent, DailyPlanComponent, PlanListComponent],
-  imports: [
-    CommonModule,
-    MatDatepickerModule,
-    MatToolbarModule, MatStepperModule, MatInputModule,
-    MatButtonModule, MatIconModule, MatListModule,
-    MatSnackBarModule, MatExpansionModule, MatTooltipModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    MatFormFieldModule,
-    MatNativeDateModule,
-    MatMenuModule
-
-
-  ],
-  exports: [
-    DailyPlanComponent
-  ]
+    declarations: [
+        PlanEditComponent,
+        PlanRunnerComponent,
+        FooterComponent,
+        NavbarComponent,
+        DailyPlanComponent,
+        PlanListComponent
+    ],
+    imports: [
+        CommonModule,
+        MatDatepickerModule,
+        MatToolbarModule,
+        MatStepperModule,
+        MatInputModule,
+        MatButtonModule,
+        MatIconModule,
+        MatListModule,
+        MatSnackBarModule,
+        MatExpansionModule,
+        MatTooltipModule,
+        ReactiveFormsModule,
+        AppRoutingModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+        MatMenuModule,
+        MatCheckboxModule
+    ],
+    exports: [DailyPlanComponent]
 })
-export class DailyPlanModule { }
+export class DailyPlanModule {}
--- src/app/daily-plan/plan-runner/plan-runner.component.ts
+++ src/app/daily-plan/plan-runner/plan-runner.component.ts
@@ -5,94 +5,86 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 
-
 @Component({
-  selector: 'app-plan-runner',
-  templateUrl: './plan-runner.component.html',
-  styleUrls: ['./plan-runner.component.scss']
+    selector: 'app-plan-runner',
+    templateUrl: './plan-runner.component.html',
+    styleUrls: ['./plan-runner.component.scss']
 })
 export class PlanRunnerComponent implements OnInit {
-  isRunning: boolean;
-  currentTaskIndex = 0; // indice del task partito dal tasto play
-  intervalID; // ogni secondo incrementa il tempo (tempotrascorso ) di 1
-  plan: Plan;
-  checked= false;
-  private routeSub: Subscription;
+    isRunning: boolean;
+    currentTaskIndex = 0; // indice del task partito dal tasto play
+    intervalID; // ogni secondo incrementa il tempo (tempotrascorso ) di 1
+    plan: Plan;
+    checked = false;
+    private routeSub: Subscription;
 
+    constructor(
+        private route: ActivatedRoute,
+        private db: PouchdbService,
+        private snackbar: MatSnackBar
+    ) {}
 
-
-  constructor(
-    private route: ActivatedRoute,
-    private db: PouchdbService,
-    private snackbar: MatSnackBar) { }
-
-  ngOnInit() {
-    this.routeSub = this.route.params.subscribe((
-      params: Params) => {
-      this.db
-        .getPlan(params.id) // prendiamo piano dal db e guarda html con for per mostrare plan
-        .then(plan => {
-          this.plan = plan;
-
-        })
-        .catch(error => {
-          console.log(error);
-          this.snackbar.open('erorr nel car dei dati', 'eroor', {
-            duration: 2000
-          });
+    ngOnInit() {
+        this.routeSub = this.route.params.subscribe((params: Params) => {
+            this.db
+                .getPlan(params.id) // prendiamo piano dal db e guarda html con for per mostrare plan
+                .then(plan => {
+                    this.plan = plan;
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.snackbar.open('erorr nel car dei dati', 'eroor', {
+                        duration: 2000
+                    });
+                });
         });
-
-    });
-  }
-  //init() {
-   // this.intervalID = null;
- // }
-
-  ngOnDestroy() {
-    this.routeSub.unsubscribe();
-  }
-
-  runTask() {
-    const task = this.plan.tasks[this.currentTaskIndex]; //currentTaskIndex è a 0
-    this.intervalID = setInterval(() => {
-      task.tempotrascorso++;
-      console.log(task.tempotrascorso);
-
-    }, 1000);
-  }
-
-  play() {
-    this.runTask();
-    this.isRunning = true;
-  }
-
-  pause() {
-    clearInterval(this.intervalID);
-    this.isRunning = false;
-  }
-
-  stop() {
-    this.pause();
-
-  }
-  // ccione iniziale per iniziare
-  togglePlayStop() {
-    if (this.isRunning) { // se è gia partito / pausa
-      this.pause();
-    } else {
-      this.play();
     }
-  }
+    //init() {
+    // this.intervalID = null;
+    // }
 
-  check() {
-    const task = this.plan.tasks[this.currentTaskIndex];
-    if(this.checked) { // checked prop [ng module], quando check il checked va a true con bana
-      task.isDone = true;
-      this.currentTaskIndex++;
-      this.runTask();
-
+    ngOnDestroy() {
+        this.routeSub.unsubscribe();
     }
-  }
 
+    runTask() {
+        const task = this.plan.tasks[this.currentTaskIndex]; //currentTaskIndex è a 0
+        this.intervalID = setInterval(() => {
+            task.tempotrascorso++;
+            console.log(task.tempotrascorso);
+        }, 1000);
+    }
 
+    play() {
+        this.runTask();
+        this.isRunning = true;
+    }
+
+    pause() {
+        clearInterval(this.intervalID);
+        this.isRunning = false;
+    }
+
+    stop() {
+        this.pause();
+    }
+    // ccione iniziale per iniziare
+    togglePlayStop() {
+        if (this.isRunning) {
+            // se è gia partito / pausa
+            this.pause();
+        } else {
+            this.play();
+        }
+    }
+
+    check() {
+        const task = this.plan.tasks[this.currentTaskIndex];
+        if (this.checked) {
+            // checked prop [ng module], quando check il checked va a true con bana
+            task.isDone = true;
+            this.currentTaskIndex++;
+            this.runTask();
+        }
+    }
 }
